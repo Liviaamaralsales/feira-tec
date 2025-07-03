c6c4870afd56f95b6c022b3be21f0cbf3df78a90
Camila Vitoria <125595804+projCamilaVitoria@users.noreply.github.com>
2025-07-03 16:19:24 -0300

    Update TelaCreditos.php

--- views/TelaCreditos.php
+++ views/TelaCreditos.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <title>Layout de Equipes</title>
-  <link rel="stylesheet" href="TelaCreditos.css">
+  <link rel="stylesheet" href="assets/CSS/TelaCreditos.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&display=swap" rel="stylesheet">
